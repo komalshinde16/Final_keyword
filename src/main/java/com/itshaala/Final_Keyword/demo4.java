0b692dff2935af45a7bc0ae38913c4347c8db924
komalshinde16 <komalambadasshinde@gmail.com>
2026-04-22 07:46:30 +0530

    demo4

---
+++ src/main/java/com/itshaala/Final_Keyword/demo4.java
@@ -0,0 +1,9 @@
+package com.itshaala.Final_Keyword;
+
+public class demo4 {
+    public static void main(String[] args) {
+        Student student = Student.getInstance();
+        System.out.println(student.id + "\t" + student.name);
+
+    }
+}
