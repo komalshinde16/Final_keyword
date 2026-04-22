a8778e4bdc7d8a622a1d98b7e177e13161bfd27c
komalshinde16 <komalambadasshinde@gmail.com>
2026-04-22 07:44:47 +0530

    i

---
+++ src/main/java/com/itshaala/Final_Keyword/Demo2.java
@@ -0,0 +1,9 @@
+package com.itshaala.Final_Keyword;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        final int a = 100;
+        //a = 200;//CTE
+        System.out.println(a);
+    }
+}
